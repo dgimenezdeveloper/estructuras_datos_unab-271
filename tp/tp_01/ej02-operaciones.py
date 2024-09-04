78a04372f9069162d019cfc722ecea265a3ec359
Daro <dgimenez.developer@gmail.com>
2024-09-04 06:04:12 -0300

    Resolución Ejercicio 2 TP 1

---
+++ tp/tp_01/ej02-operaciones.py
@@ -0,0 +1,37 @@
+num1 = 5
+num2 = 2
+
+resul = num1 // num2
+print(resul) #2 Division entera truncado.
+
+###########################
+num1 = 7.2
+num2 = 9.8
+
+resul = num1 * num2
+print(resul) # 70.56
+
+###########################
+
+num1 = 7
+num2 = 3.1
+
+resul = num1 + num2
+print(resul) # 10.1 
+
+############################
+
+num1 = 'republica'
+num2 = 'argentina'
+
+resul = num1 + num2
+print(resul) # republicaargentina
+
+############################
+
+num1 = 'argentina'
+num2 = 3
+
+resul = num1 * num2
+print(resul) # argentinaargentinaargentina
+
