d628964ee7ca76669bd2c020ec35de481d62874e
Daro <dgimenez.developer@gmail.com>
2024-09-06 19:16:05 -0300

    Se refactoriza usando clases

--- tp/tp_01/ej-05-fracciones.py
+++ tp/tp_01/ej-05-fracciones.py
@@ -4,18 +4,25 @@ a) Escribir un programa que sume dos fracciones. Para ello el usuario deberá in
 b) Escribir un programa que reste dos fracciones. Para ello el usuario deberá ingresar los datos de las dos fracciones por teclado. 
 c) Escribir un programa que multiplique dos fracciones. Para ello el usuario deberá ingresar los datos de las dos fracciones por teclado. 
 d) Escribir un programa que divida dos fracciones. Para ello el usuario deberá ingresar los datos de las dos fracciones por teclado. """
+class Fraccion:
+    def __init__(self, num, den):
+        self.num = num
+        self.den = den
 
-def sumar(fracc1, fracc2):
-    return fracc1[0] + fracc2[0], fracc1[1] + fracc2[1]
+    def sumar(self, fraccion):
+        return self.num * fraccion.den + fraccion.num * self.den, self.den * fraccion.den
 
-def restar(fracc1, fracc2):
-    return fracc1[0] - fracc2[0], fracc1[1] - fracc2[1]
+    def restar(self, fraccion):
+        return self.num * fraccion.den - fraccion.num * self.den, self.den * fraccion.den
 
-def multiplicar(fracc1, fracc2):
-    return fracc1[0] * fracc2[0], fracc1[1] * fracc2[1]
+    def multiplicar(self, fraccion):
+        return self.num * fraccion.num, self.den * fraccion.den
 
-def dividir(fracc1, fracc2):
-    return fracc1[0] * fracc2[1], fracc1[1] * fracc2[0]
+    def dividir(self, fraccion):
+        return self.num * fraccion.den, self.den * fraccion.num
+
+    def __str__(self):
+        return f'{self.num}/{self.den}'
 
 def obtener_datos():
     num = int(input('Ingresa el numerador: '))
@@ -23,17 +30,13 @@ def obtener_datos():
     return num,den
 
 def main():
-    fracc1 = obtener_datos()
-    fracc2 = obtener_datos()
-    
-    suma = sumar(fracc1, fracc2)
-    resta = restar(fracc1, fracc2)
-    producto = multiplicar(fracc1, fracc2)
-    division = dividir(fracc1, fracc2)
-    
-    print(f'Suma: {suma}')
-    print(f'resta: {resta}')
-    print(f'Producto: {producto}')
-    print(f'División: {division}')
+    num1, den1 = obtener_datos()
+    num2, den2 = obtener_datos()
+    fraccion1 = Fraccion(num1, den1)
+    fraccion2 = Fraccion(num2, den2)
+    print(f'La suma de las fracciones es: {fraccion1.sumar(fraccion2)}')
+    print(f'La resta de las fracciones es: {fraccion1.restar(fraccion2)}')
+    print(f'La multiplicación de las fracciones es: {fraccion1.multiplicar(fraccion2)}')
+    print(f'La división de las fracciones es: {fraccion1.dividir(fraccion2)}')
 
 main()
