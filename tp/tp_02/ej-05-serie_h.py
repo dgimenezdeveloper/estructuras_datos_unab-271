e6852f3146e217b5449cd89c6ce96c4edf8cd4fe
Daro <dgimenez.developer@gmail.com>
2024-09-06 19:16:26 -0300

    Se modifica función iterativo a recursiva

--- tp/tp_02/ej-05-serie_h.py
+++ tp/tp_02/ej-05-serie_h.py
@@ -2,12 +2,12 @@
 1+1/2+1/3+...+1/n """
 
 def serie_h(n):
-    suma = 0
-    for i in range(1, n + 1):
-        suma += 1 / i
-    return suma
+    if n == 1:
+        return 1
+    else:
+        return 1/n + serie_h(n-1)
 
 def main():
-    print(serie_h(5)) # 2.283333333333333
+    print(serie_h(5))
 
 main()
